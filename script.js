7741f941a643141e5475be0e828ae36565e6f32c
Ryan Macalandag <rmacalandag@gmail.com>
2024-01-31 23:05:45 +1100

    fetch from local data.json

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const selectedSociality = document.querySelector('.dropdown.sociality .selected-
 const filtersButton = document.querySelector('.dropdown.filters');
 const filtersContainer = document.querySelector('#filter-styles');
 
+// Filter bar
 let currentSociality = "All";
 
 console.log(dropDownItems)
@@ -22,7 +23,6 @@ dropDown.forEach((i) => {
     thisDropDown.classList.toggle('close');
 
     dropDownItems.forEach((j) => {
-      console.log(j.dataset.sociality)
       j.addEventListener('click', (e) => {
         setCurrentSociality(j.dataset.sociality);
       })
@@ -40,3 +40,49 @@ themeToggleContainer.forEach((i) => {
     DOMbody.classList.toggle('dark')
   })
 })
+
+// Fetch data
+async function getGalleryData() {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  return data;
+}
+
+getGalleryData().then((data) => {
+  const galleryItems = data;
+
+  // Create main gallery from data using template
+  const mainGallery = document.querySelector('#main-gallery');
+  const itemTemplate = document.querySelector('#item-template');
+
+  let galleryList = document.createElement('ul');
+  galleryList.classList.add('gallery');
+  galleryList.classList.add('boxed');
+
+  galleryItems.forEach((item) => {
+
+    // Clone inner content of template
+    // Then grab li element
+    const li = itemTemplate.content.cloneNode(true).querySelector('li');
+    const img = li.querySelector('img');
+
+    // Fill gallery item data
+    li.dataset.sociality = item.sociality;
+    li.dataset.category = item.category;
+    li.dataset.tags = item.tags;
+    li.dataset.medium = item.tags;
+
+    // Fill gallery image data
+    img.src = item.url;
+    img.alt = item.title;
+
+    // Append li item to ul list
+    galleryList.append(li);
+
+  })
+
+  // Append ul list to main gallery
+  mainGallery.append(galleryList);
+
+})
+
